0c8413f007ec2dbffb53315f7df0cde0dface42c
Christoph <christoph@olamalu.com>
2022-06-28 16:29:57 +0100

    Add loading and search text indicators.

--- src/pages/collections.tsx
+++ src/pages/collections.tsx
@@ -10,6 +10,7 @@ import { FooterLogo, FooterProps, SocialMedia } from "../components/footer"
 import MiniatureItem, { MiniatureItemInterface } from "../components/miniatureItem"
 import { buildDirectusRequestUrl, createSearchIndex } from "../util/search"
 import { Index } from "lunr"
+import Loading from '../images/loading-spin.svg'
 
 
 interface MiniatureItemWithSearchResultInterface {
@@ -27,6 +28,8 @@ const CollectionsPage = ({data}: PageProps<Queries.SiteDataQuery>) => {
       footerLogos: data.site?.siteMetadata?.footerLogos ? data.site?.siteMetadata?.footerLogos as Array<FooterLogo> : []
     }
   }
+  const [loading, setLoading] = useState(1)
+  const [searchText, setSearchText] = useState('')
   const [miniatures, setMiniatures] = useState<Map<string, MiniatureItemInterface>>()
   const [searchIndex, setSearchIndex] = useState<Index>()
   const [filteredMiniatures, setFilteredMiniatures] = useState<Array<MiniatureItemWithSearchResultInterface>>([])
@@ -45,11 +48,14 @@ const CollectionsPage = ({data}: PageProps<Queries.SiteDataQuery>) => {
         setFilteredMiniatures(filtered)
         const idx = createSearchIndex(resultData.data)
         setSearchIndex(idx)
+        setLoading(0)
       })
   }, [])
 
     function handleSearchKeywords(event: React.ChangeEvent<HTMLInputElement>) {
+      setLoading(1)
       const searchText = event.target.value
+      setSearchText(searchText)
       const results = searchIndex?.search(searchText)
       const filtered: Array<MiniatureItemWithSearchResultInterface> = []
       results?.forEach((result) => {
@@ -59,6 +65,7 @@ const CollectionsPage = ({data}: PageProps<Queries.SiteDataQuery>) => {
         }
       })
       setFilteredMiniatures(filtered)
+      setLoading(0)
     }
    
     
@@ -69,6 +76,12 @@ const CollectionsPage = ({data}: PageProps<Queries.SiteDataQuery>) => {
           <div className={`miniature-items-search`}>
             <div className="search-label">Search for artist, sitter or pigment</div>
             <input name="searchKeywords" onChange={handleSearchKeywords} placeholder="Search"/>
+          </div>
+          {(searchText && filteredMiniatures.length == 0) && (<div className={`no-search-results`}>
+            <span className="no-search-results--text">Searching for <span className="search-text">{searchText}</span>.    No results yet.   Please keep typing or search another term.</span>
+          </div>)}
+          <div className="loading">
+            {loading !=0 && <Loading/>}
           </div>
           <div className={`miniature-items`}>
             {Array.isArray(filteredMiniatures) && filteredMiniatures.map(item => {
